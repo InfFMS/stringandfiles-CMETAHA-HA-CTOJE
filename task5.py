91a72ae6f2fda8a284416a00f9c8b7046b9db221
yaroslav.alelekov <yaroslav.alelekov@gmail.com>
2025-01-27 16:01:31 +0700

    Пятое задание

--- task5.py
+++ task5.py
@@ -5,3 +5,27 @@
 # Его длина: длина
 #
 # Выведите это слово и длину в консоль.
+
+import string
+punc = string.punctuation
+space = string.whitespace
+lst = punc + space
+with open('task5.txt', encoding='utf-8') as file:
+    max_len = 0
+    word = []
+    max_word = []
+    for line in file:
+        count = 0
+        line = list(line)
+        for i in line:
+            if i in lst:
+                if count > max_len:
+                    max_len = count
+                    max_word = word
+                word = []
+                count = 0
+            else:
+                count += 1
+                word.append(i)
+print(f'Самое длинное слово: {''.join(max_word)}')
+print(f'Его длина: {max_len}')
